db1be573f4fa8df29ca50c17778e56080a9b9301
Aashaar Panchalan <aashaar@outlook.com>
2019-03-30 05:48:55 -0500

    created form for user and printed user's input on console

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,16 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=boston').then((response)=>{
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const location = search.value
+    console.log(location)
+
+
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
     response.json().then((data) =>{
         if(data.error)
         {
@@ -12,4 +22,6 @@ fetch('http://localhost:3000/weather?address=boston').then((response)=>{
             console.log(data.forecast)
         }
     })
+})
+
 })
