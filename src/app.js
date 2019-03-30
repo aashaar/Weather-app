bfa7542ef2c1bd289b59a66045184e39d6c51908
Aashaar Panchalan <aashaar@outlook.com>
2019-03-30 04:33:45 -0500

    fixed destructuring bug by adding an empty object as default value in geocode's call

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// ###################################
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
@@ -68,6 +69,7 @@ app.get('/weather',(req,res)=>{
                error //short hand for error:error
            }) 
         }
+
         forecast(latitude,longitude,(error, forecastData)=>{
             if(error)
             {
